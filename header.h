e24f3eae4e98a9c379a7f97305f68cf7c7ba1da2
shravanimula <98573934+shravanimula@users.noreply.github.com>
2022-04-29 17:21:34 +0530

    headerfile

---
+++ header.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+void check_set(int,int);
+void check_clr(int,int);
+void check_get(int,int);
+void check_toggle(int,int);
+void check_swap(int,int);
+void check_least(int);
+void check_most(int);
