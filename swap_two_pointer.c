ea7431563aa7082bcd8a652d03e2f2625a157075
shravanimula <98573934+shravanimula@users.noreply.github.com>
2022-04-29 18:10:51 +0530

    swap

---
+++ swap_two_pointer.c
@@ -0,0 +1,13 @@
+#include"swap.h"
+int main()
+{
+	int num1,num2;
+	printf("Enter value of num1: ");
+	scanf("%d",&num1);
+	printf("Enter value of num2: ");
+	scanf("%d",&num2);
+	printf("Before Swapping: num1 is: %d, num2 is: %d\n",num1,num2);
+	swap(&num1,&num2);
+	printf("After  Swapping: num1 is: %d, num2 is: %d\n",num1,num2);
+	return 0;
+}
